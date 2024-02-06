b59450d99bdd63157564211e567a5ed1a9765f0e
shahabas <shahabassabbu12@gmail.com>
2024-02-06 23:14:10 +0530

    Materials Updated

---
+++ BCA/SEMESTER_4/Python Lab/Part A/String-List Swapping and combining.py
@@ -0,0 +1,20 @@
+""""
+2. Perform the following operations on string list:
+    a) Swap elements of string list
+    b) Combining two list
+"""
+x="Srinivas" 
+y="University"
+
+print(x)
+a=list(x)
+print(a)
+
+print(y)
+b=list(y)
+print(b)
+
+print(a[::-1])
+print(b[::-1])
+
+print(a+b)
