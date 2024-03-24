21f4747ebdcdeabac876c3326376c17f6c7ab3e0
shahabas <shahabassabbu12@gmail.com>
2024-03-25 01:25:18 +0530

    check

--- pages/view.php
+++ pages/view.php
@@ -28,7 +28,7 @@ if (isset($_GET['file'])) {
         <link rel="shortcut icon" type="image/png" sizes="16x16" href="https://duploader.tech/assets/favicon/android-chrome-192x192.png?v=1706301104">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="../assets/script/navbar.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/prismjs@1.25.0/prism.min.js"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/prismjs@1.25.0/prism.min.js"></script>
         <style>
             /* Custom CSS for PDF container */
             #pdfContainer {
