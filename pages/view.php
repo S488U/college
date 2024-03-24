d6b4212853e59551cfa434b749c1c3863a4afbe0
shahabas <shahabassabbu12@gmail.com>
2024-03-25 01:10:33 +0530

    formatted

--- pages/view.php
+++ pages/view.php
@@ -127,7 +127,8 @@ if (isset($_GET['file'])) {
                 if (pathinfo($fileLocation, PATHINFO_EXTENSION) === 'pdf') {
                     $partToRemove = "/var/www/vhosts/duploader.tech/";
                     $resultLink = str_replace($partToRemove, '', $fileLocation);
-                    echo "<iframe src='https://$resultLink' width='100%' height='500'></iframe>";
+                    $encodedLink = rawurlencode("https://$resultLink");
+                    echo "<iframe src='$encodedLink' width='100%' height='500'></iframe>";
                     // echo "<div id='pdfContainer'></div>";
                     // echo "<script src='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.min.js'></script>";
                     // echo "<script>
