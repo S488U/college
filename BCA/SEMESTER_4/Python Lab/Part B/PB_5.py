ebb3bc920c5be2773a060d4d5d1da6f6f6288c6a
Shahabas Abdul Hameed <96893016+S488U@users.noreply.github.com>
2024-04-08 12:17:59 +0530

    Update PB_5.py
    Updated Python part b 5th program

--- BCA/SEMESTER_4/Python Lab/Part B/PB_5.py
+++ BCA/SEMESTER_4/Python Lab/Part B/PB_5.py
@@ -3,46 +3,28 @@
 
 
 import pymongo
-
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 
 mydb = myclient['mydatabase']
-
 mycol = mydb["customers"]
 
 mylist = [
   { "name": "Amy", "address": "Apple st 652"},
   { "name": "Hannah", "address": "Mountain 21"},
-  { "name": "Michael", "address": "Valley 345"},
-  { "name": "Sandy", "address": "Ocean blvd 2"},
-  { "name": "Betty", "address": "Green Grass 1"},
-  { "name": "Richard", "address": "Sky st 331"},
-  { "name": "Susan", "address": "One way 98"},
-  { "name": "Vicky", "address": "Yellow Garden 2"},
-  { "name": "Ben", "address": "Park Lane 38"},
-  { "name": "William", "address": "Central st 954"},
-  { "name": "Chuck", "address": "Main Road 989"},
-  { "name": "Viola", "address": "Sideway 1633"}
+  { "name": "Michael", "address": "Valley 345"}
 ]
 
 x = mycol.insert_many(mylist)
 
-print(x)
-
+print("Printing all the records: ")
 for x in mycol.find():
   print(x)
 
-for x in mycol.find({},{ "_id": 0, "name": 1, "address": 1 }):
-  print(x)
+print("Printing only the name and address of all the records: ")
+for y in mycol.find({},{ "_id": 0, "name": 1, "address": 1 }):
+  print(y)
 
-myquery = { "address": "Valley 345" }
-newvalues = { "$set": { "address": "Canyon 123" } }
 
-mycol.update_one(myquery, newvalues)
-
-#print "customers" after the update:
-for x in mycol.find():
-  print(x)
-myquery = { "address": "Mountain 21" }
-
-mycol.delete_one(myquery)
+print("Printing single record: ")
+z=mycol.find_one()
+print(z)
