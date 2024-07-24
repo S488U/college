07d2b0b749885019ef860eabdbccebf52e3a0855
shahabas <shahabassabbu12@gmail.com>
2024-07-25 00:45:06 +0530

    Google Analytics setup

--- pages/error.php
+++ pages/error.php
@@ -10,6 +10,7 @@
     <link rel="shortcut icon" type="image/png" sizes="16x16" href="https://duploader.tech/assets/favicon/android-chrome-192x192.png?v=1706301104">
     <link rel="stylesheet" href="./assets/style/scrollbar.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <?php include "./g-tag.php"; ?>
 </head>
 
 <body>
