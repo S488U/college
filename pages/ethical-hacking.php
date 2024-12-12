a3d6038bbfedac748371d1537abee0d3eae3c4bd
shahabas <shahabassabbu12@gmail.com>
2024-12-12 14:20:31 +0530

    changes

--- pages/ethical-hacking.php
+++ pages/ethical-hacking.php
@@ -29,7 +29,7 @@ include "/var/www/vhosts/dunite.tech/components/db.php";
         <h1 class="text-capitalize">ethical Hacking Training</h1>
         <div class="container-fluid d-flex flex-column justify-content-center align-items-center ">
             <div class="container py-5">
-                <div class="row row-cols-1 row-cols-md-2 g-3 ">
+                <div class="row row-cols-1 row-cols-md-2 g-2 g-md-3">
                     <?php
                     $sql = "SELECT * FROM courses";
                     $result = $conn->query($sql);
