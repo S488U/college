1481c17fa86946963cbd6d3062d2aeac1ce0c33f
christo790 <christosgeorge0@gmail.com>
2025-11-23 12:05:11 +0530

    Add p1.c file

---
+++ BCA/SEMESTER_1/C/C Lab/p1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a, b, c, large;
+    printf("Enter the numbers");
+    scanf("%d%d%d", &a, &b, &c);
+    large = a;
+    if (b > large)
+    {
+        large = b;
+    }
+    if (c > large)
+    {
+        large = c;
+    }
+    printf("The largest number id %d", large);
+}
