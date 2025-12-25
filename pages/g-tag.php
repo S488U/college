e41d91e3ccb9b68b9a48332dc96cb25a9b42c23c
S488U <shahabassabbu12@gmail.com>
2025-12-25 21:50:24 +0530

    style(g-tag): standardize indentation for tracking scripts
    Applies consistent tab indentation to the Google Tag and AdSense scripts within the g-tag tracking file.
    
    Signed-off-by: S488U <shahabassabbu12@gmail.com>

--- pages/g-tag.php
+++ pages/g-tag.php
@@ -1,12 +1,12 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-9RZ7EVD4TN"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-9RZ7EVD4TN"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
 
-  gtag('config', 'G-9RZ7EVD4TN');
-</script>
+	  gtag('config', 'G-9RZ7EVD4TN');
+	</script>
 
-<!-- Google AdSense -->
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2144590426473210" crossorigin="anonymous"></script>
+	<!-- Google AdSense -->
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2144590426473210" crossorigin="anonymous"></script>
